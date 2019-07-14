8d81b3afc9d60615ef92bfef137e2f5c7f7cf73b
Andres Lujan <andreslujandev@gmail.com>
2019-07-14 17:46:10 -0500

    WIP Stack

--- data_structures/queue/queue.py
+++ data_structures/queue/queue.py
@@ -8,6 +8,23 @@ class Queue:
         self.front_index = -1
         self.queue_size = 0
 
+    def enqueue(self, value):
+        self.arr[self.nex_index] = value
+        self.queue_size += 1
+        self.nex_index += (self.nex_index + 1) % len(self.arr)
+
+        if self.front_index == -1:
+            self.front_index = 0
+
+    def size(self):
+        return self.queue_size
+    
+    def is_empty(self):
+        return self.size() == 0
+    
+    def front(self):
+        return arr[self.front_index]
+
 
 if __name__ == '__main__':
     q = Queue()

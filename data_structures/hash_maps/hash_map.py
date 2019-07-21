ce4e8e40a2ac4c22c64ba604303f7be896ae6b5a
Andres Lujan <andreslujandev@gmail.com>
2019-07-21 18:38:53 -0500

    WIP hashing maps

--- data_structures/hash_maps/hash_map.py
+++ data_structures/hash_maps/hash_map.py
@@ -27,3 +27,24 @@ class HashTable:
     def calculate_hash_value(self, string):
         value = ord(string[0])*100 + ord(string[1])
         return value
+
+# Setup
+hash_table = HashTable()
+
+# Test calculate_hash_value
+# Should be 8568
+print (hash_table.calculate_hash_value('UDACITY'))
+
+# Test lookup edge case
+# Should be -1
+print (hash_table.lookup('UDACITY'))
+
+# Test store
+hash_table.store('UDACITY')
+# Should be 8568
+print (hash_table.lookup('UDACITY'))
+
+# Test store edge case
+hash_table.store('UDACIOUS')
+# Should be 8568
+print (hash_table.lookup('UDACIOUS'))

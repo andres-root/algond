66555722363aa84e7823a71285999e40abcd86c9
Andres Lujan <andreslujandev@gmail.com>
2019-07-21 19:09:55 -0500

    WIP hashing maps

--- data_structures/hash_maps/hash_map.py
+++ data_structures/hash_maps/hash_map.py
@@ -3,7 +3,7 @@
 
 class HashMap:
     def __init__(self, initial_size=10):
-        self.bucket = [None for _ in range(initial_size)]
+        self.bucket_array = [None for _ in range(initial_size)]
         self.prime = 37
         self.num_entries = 0
     
@@ -14,27 +14,32 @@ class HashMap:
         pass
     
     def get_bucket_index(self, key):
+        print(self.get_hash_code(key), 'Index')
         return self.get_hash_code(key)
     
     def get_hash_code(self, key):
         key = str(key)
-        num_buckets = len(self.bucket)
+        num_buckets = len(self.bucket_array)
         current_coefficient = 1
         hash_code = 0
 
         for c in key:
             hash_code += ord(c) * current_coefficient
+            hash_code = hash_code % num_buckets  # Compress hash_code
             current_coefficient = self.prime
+            current_coefficient = current_coefficient % num_buckets  # Compress coefficient
 
-        return hash_code
+        return hash_code % num_buckets  # One last compression before returning
 
-# Testing
-hash_map = HashMap()
 
-bucket_index = hash_map.get_bucket_index('abcd')
-print(bucket_index)
+if __name__ == '__main__':
+    # Testing
+    hash_map = HashMap()
 
-hash_map = HashMap()
+    bucket_index = hash_map.get_bucket_index('abcd')
+    print(bucket_index)
 
-bucket_index = hash_map.get_bucket_index('bcda')
-print(bucket_index)
+    hash_map = HashMap()
+
+    bucket_index = hash_map.get_bucket_index('bcda')
+    print(bucket_index)

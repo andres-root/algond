d0a74a570775b80802b80b66ffd21ac41aeb207a
Andres Lujan <andreslujandev@gmail.com>
2019-07-21 23:38:57 -0500

    WIP algoritms

--- algoritms/pair_sum/solution.py
+++ algoritms/pair_sum/solution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-def pair_sum(arr, t):
+def pair_sum_sorted(arr, t):
     l = 0
     r = len(arr) - 1
 
@@ -17,9 +17,25 @@ def pair_sum(arr, t):
 
     return False
 
+def pair_sum(arr, t):
+    mem = {}
+    l = 0
+    h = len(arr)
+    mem[arr[l]] = l
+
+    for i in range(1, h):
+        d = t - arr[i]
+
+        if d in mem.keys():
+            return (mem[d], i)
+
+        mem[arr[i]] = i
+    
+    return False
+
 
 if __name__ == '__main__':
-    arr = [1, 2, 3, 4, 5, 6, 7, 9, 10]
-    t = 12
+    arr = [3, 4, 6, 7, 12, 14, 2, 1, 0, 9]
+    t = 26
     pair = pair_sum(arr, t)
     print(pair)

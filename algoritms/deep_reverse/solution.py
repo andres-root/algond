c748c583c0b351db43abc3cac31ef1c16ad18561
Andres Lujan <andreslujandev@gmail.com>
2019-08-05 14:40:56 -0500

    WIP algoritms

--- algoritms/deep_reverse/solution.py
+++ algoritms/deep_reverse/solution.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
+def is_list(element):
+    """
+    Check if element is a Python list
+    """
+    return isinstance(element, list)
 
+def deep_reverse_udacity(arr):
+    """
+    Function to deep_reverse an input list
+    """
+    return deep_reverse_func(arr, 0)
+
+def deep_reverse_func(arr, index):
+    """
+    Recursive function to deep_reverse the input list
+    """
+    # Base Case
+    if index == len(arr):
+        return list()
+    
+    output = deep_reverse_func(arr, index + 1)
+    
+    # if element is a list --> deep_reverse the list
+    if is_list(arr[index]):
+        to_append = deep_reverse(arr[index])
+    else:
+        to_append = arr[index]
+        
+    output.append(to_append)
+    return output
+
+
+# My solutions
 def deep_reverse(arr):
     return reverse(arr)
 
